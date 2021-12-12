8c81c3106f7a937198d77cda0b37ac68c9b96c62
Vaillant jeremy <vaillant.jeremy@dev-crea.com>
2021-12-12 16:26:12 +0100

    Chnge default scene

--- scenes/menus/Home.gd
+++ scenes/menus/Home.gd
@@ -4,7 +4,8 @@ func _ready():
 	$CenterContainer/HBoxContainer/menu/version.text = Constants.VERSION
 
 func _on_start_pressed():
-	pass # Replace with function body.
+	# warning-ignore:return_value_discarded
+	get_tree().change_scene("res://scenes/Main.tscn")
 
 func _on_quit_pressed():
 	get_tree().quit() 
