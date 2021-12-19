314a2d774e994aa8e227559accdd23e74900c46e
Vaillant jeremy <vaillant.jeremy@dev-crea.com>
2021-12-19 17:03:35 +0100

    Add UI for chatbox

--- scenes/menus/ServerMultiplayer.gd
+++ scenes/menus/ServerMultiplayer.gd
@@ -5,3 +5,8 @@ func _ready():
 	var peer = NetworkedMultiplayerENet.new()
 	peer.create_server(Constants.SERVER_PORT, Constants.MAX_PLAYERS)
 	get_tree().network_peer = peer
+	
+	if get_tree().is_network_server():
+		print("Is network server !")
+	else:
+		print("Is not network server")
